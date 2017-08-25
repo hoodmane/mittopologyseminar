73b205a5c98effaf92f72fe46d55298ab5cba0e2
Eva <ekbelmont@gmail.com>
2017-08-25 16:25:49 -0400

    More aggressive fix-permissions.sh

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
-find -not \( -path "./node_modules" -prune \) -not \( -path "./.git" -prune \)  -user $UID -exec chmod g+rw {} \; -exec chown :topology {} \; 2>/dev/null
+# Finds files that have the wrong group ownership (should be g+w, owned by
+# topology) and tries to take over ownership, using the assumption that you
+# probably have write access to the directory, if not to the file. This will
+# fail if you don't have write access to the directory.
+export groupname="topology"
+
+reown(){
+    tmpfile="$1".tmp
+    while [[ -e "$tmpfile" ]] ; do
+        tmpfile="$tmpfile".tmp
+    done
+    if cp "$1" "$tmpfile" 2>/dev/null ; then
+        if mv -f "$tmpfile" "$1" ; then
+            # success
+            chown :"$groupname" "$1"
+            chmod g+w "$1"
+            echo "Fixed permissions on $1"
+        else  # made copy but failed to move it into place
+            rm "$tmpfile"
+        fi
+    else
+        echo "Failed to change permissions on $1"
+    fi
+}
+
+# need to export this so it can be used in a subshell in the find -exec
+export -f reown
+
+find \( -path ./node_modules -prune \) , \( -path ./.git -prune \) , -type f \! \( -perm -g=w -a -group "$groupname" \) -exec bash -c 'reown "$0"' {} \;
